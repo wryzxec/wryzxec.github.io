d8681aa45145f7b2ea2e350074763fb39f00c39f
Lucas Bruckbauer <lucas.bruckbauer@gmail.com>
2024-07-19 18:31:17 +0100

    Modified Results Table Layout

--- src/components/styles/table/table.js
+++ src/components/styles/table/table.js
@@ -3,18 +3,18 @@ import './table.css';
 
 // Sample data for the table
 const data = [
-  { Epochs: 20, Mini_Batch_Size: 128, Neurons: '200/100/25/10', Learning_Rate: 0.5, Momentum: 'True', Accuracty: 98.42 },
-  { Epochs: 20, Mini_Batch_Size: 128, Neurons: '100/50/10', Learning_Rate: 0.5, Momentum: 'True', Accuracty: 98.14 },
-  { Epochs: 20, Mini_Batch_Size: 128, Neurons: '100/50/10', Learning_Rate: 0.5, Momentum: 'False', Accuracty: 97.51},
-  { Epochs: 20, Mini_Batch_Size: 60000, Neurons: '100/50/10', Learning_Rate: 0.5, Momentum: 'True', Accuracty: 84.13},
-  { Epochs: 20, Mini_Batch_Size: 60000, Neurons: '100/50/10', Learning_Rate: 0.5, Momentum: 'False', Accuracty: 65.16},
+  { id: 1, Epochs: 20, Mini_Batch_Size: 128, Neurons: '200/100/25/10', Learning_Rate: 0.5, Momentum: 'True', Accuracy: 98.42 },
+  { id: 2, Epochs: 20, Mini_Batch_Size: 128, Neurons: '100/50/10', Learning_Rate: 0.5, Momentum: 'True', Accuracy: 98.14 },
+  { id: 3, Epochs: 20, Mini_Batch_Size: 128, Neurons: '100/50/10', Learning_Rate: 0.5, Momentum: 'False', Accuracy: 97.51},
+  { id: 4, Epochs: 20, Mini_Batch_Size: 60000, Neurons: '100/50/10', Learning_Rate: 0.5, Momentum: 'True', Accuracy: 84.13},
+  { id: 5, Epochs: 20, Mini_Batch_Size: 60000, Neurons: '100/50/10', Learning_Rate: 0.5, Momentum: 'False', Accuracy: 65.16},
 ];
 
 // Table component
 const Table = () => {
   return (
     <div className="table-container">
-      <table>
+      <table className="table">
         <thead>
           <tr>
             <th>Epochs</th>
@@ -22,7 +22,7 @@ const Table = () => {
             <th>Neurons (Layer 1/Layer 2/.../Layer n)</th>
             <th>Learning Rate</th>
             <th>Momentum Applied</th>
-            <th> Accuracy (%) </th>
+            <th>Accuracy (%)</th>
           </tr>
         </thead>
         <tbody>
@@ -33,7 +33,7 @@ const Table = () => {
               <td>{item.Neurons}</td>
               <td>{item.Learning_Rate}</td>
               <td>{item.Momentum}</td>
-              <td>{item.Accuracty}</td>
+              <td>{item.Accuracy}</td>
             </tr>
           ))}
         </tbody>
